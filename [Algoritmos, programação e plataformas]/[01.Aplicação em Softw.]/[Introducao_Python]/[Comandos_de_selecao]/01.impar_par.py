e803b6a7f99ed70d0243e8c89a34b63c274bb311
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-03-04 09:02:56 -0300

    Comandos de seleção

--- [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comandos_de_selecao]/01.impar_par.py
+++ [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comandos_de_selecao]/01.impar_par.py
@@ -1,11 +1,11 @@
 # Elabore um programa que leia um nº e o transforme
 # em impar com a adição de 1 
 
-print("Ímpar ou par!")
+print("Impar ou par!")
 
 num = int(input(f"\nDigite um número: "))
 
 if num % 2 == 0:
-    print(f"\nSeu numero foi transformado em ímpar, {num+1} ")
+    print(f"\nSeu numero foi transformado em impar: {num+1}! ")
 else:
-    print(F"\nSeu número é ímpar")
+    print(F"\nSeu número é impar: {num}")
