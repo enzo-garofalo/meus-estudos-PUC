3e28693fe93c8502edeb813bae93886215661443
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-03-04 09:34:18 -0300

    Comandos de seleção 2

--- [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comandos_de_selecao]/07.desafio_media.py
+++ [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comandos_de_selecao]/07.desafio_media.py
@@ -10,7 +10,7 @@ print(f"\n\tMédia!")
 n1 = float(input(f"\nDigite a nota de peso 1: "))
 n2 = float(input(f"Digite a nota de peso 2: "))
 
-media = ((n1*0.4) + (n2*0.6))
+media = (n1*0.4) + (n2*0.6)
 
 if media >= 7 and media < 10:
     print(F"\tO aluno está aprovado!")
