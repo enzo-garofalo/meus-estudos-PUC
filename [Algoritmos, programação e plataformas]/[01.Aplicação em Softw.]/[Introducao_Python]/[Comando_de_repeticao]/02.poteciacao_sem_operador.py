b98a99a4cb568a1fb88be5c0a12ad98dde25ee2d
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-03-11 10:22:49 -0300

    Comando de repetição

---
+++ [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comando_de_repeticao]/02.poteciacao_sem_operador.py
@@ -0,0 +1,18 @@
+# Escreva um programa que calcule x elevado a n. Considere que n é um
+# # valor inteiro não negativo. PROIBIDO USAR QUALQUER FUNÇÃO MATEMATICA EXISTENTE no PYTHON
+
+print(f"\tPotenciação sem operadores!")
+
+x = int(input("Digite o valor a ser elevado: "))
+n = int(input("Digite a potencia: "))
+resultado = 1
+i = 1
+
+print(f"{x**n}")
+
+while i <= n:
+    resultado = resultado * x
+    i += 1
+
+print(f"O Valor de {x} elevado a {n} é {resultado}")
+
---
+++ [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comando_de_repeticao]/03.multiplos_de_sete.py
@@ -0,0 +1,7 @@
+i = 0
+while i <= 450:
+    if i%7 == 0:
+        print(f"{i}")
+        i += 1
+    else:
+        i += 1
