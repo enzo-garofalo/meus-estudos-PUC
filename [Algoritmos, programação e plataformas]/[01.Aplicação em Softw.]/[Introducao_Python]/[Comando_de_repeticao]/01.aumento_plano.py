12320c6c3c4f1f2871b5a5ef2fbd0987357bb0f0
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-03-11 21:52:42 -0300

    simple

--- [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comando_de_repeticao]/01.aumento_plano.py
+++ [Algoritmos, programação e plataformas]/[01.Aplicação em Softw.]/[Introducao_Python]/[Comando_de_repeticao]/01.aumento_plano.py
@@ -7,29 +7,8 @@
 # Faça um programa que leia o plano de trabalho e o salário atual de 
 # um funcionário e calcula e imprime o seu novo salário. 
 
-print(f"\n\t<<<<<<<<LEMBRETE ESTUDAR PIP INSTALL PARA BILBIOTECAS NO GIT>>>>>>>>")
-# from rich.console import Console
-# from rich.table import Table
-
-# table = Table(title="Planos")
-
-# rows = [
-#     [1, "10%"],
-#     [2, "15%"],
-#     [3, "20%"],
-# ]
-
-# columns = ["PLANOS", "AUMENTO"]
-
-# for column in columns:
-#     table.add_column(column)
-
-# for row in rows:
-#     table.add_row(*row)
-
-# console = Console()
-# Console.print(table)
 print(f"\n\tPlanos de aumento")
+
 plano = int(input("\nDigite o seu plano de aumento: "))
 salario = float(input("Digite o salário base: "))
 
