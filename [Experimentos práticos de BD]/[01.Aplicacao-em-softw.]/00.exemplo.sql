50e2378d98d67467ffe825d4092ce2027e743ebf
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-04-08 18:43:12 -0300

    Aulas EXPBD

--- [Experimentos práticos de BD]/[01.Aplicacao-em-softw.]/00.exemplo.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE alunos
-(
-    RA number (8) NOT NULL PRIMARY KEY,
-    nome VARCHAR2(50),
-    idade number (2),
-    cidade VARCHAR2(50)
-);
--- [Experimentos práticos de BD]/[01.Aplicacao-em-softw.]/[Locadora]/01.Cliente.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE cliente
-(
-    id_cliente NUMBER(3) NOT NULL PRIMARY KEY,
-    nome VARCHAR2(50) NOT NULL
-)
