cb6464c4dc3879ed64297141745a598749236921
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-05-05 18:27:51 -0300

    Fix: odd word bug

--- [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/Cifra-de-Hill.py
+++ [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/Cifra-de-Hill.py
@@ -13,7 +13,7 @@ def criptografar():
 
   matriz_palavra_em_num = palavra_em_matriz(palavra)
 
-  palavra = palavra[:-1]
+  if palavra_impar is True: palavra = palavra[:-1]
   print("-"*50)
   print(f"Matriz da palavra  {palavra} em número:")
   print(matriz_palavra_em_num)
