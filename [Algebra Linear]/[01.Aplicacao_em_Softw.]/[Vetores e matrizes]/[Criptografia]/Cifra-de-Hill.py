889a906639894c950693f197adff7ca9ca5ab579
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-05-05 17:32:32 -0300

    Fix: separando funções

--- [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/Cifra-de-Hill.py
+++ [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/Cifra-de-Hill.py
@@ -1,25 +1,19 @@
 import numpy as np
 import os
+alfabeto = {' ': 0,'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
 
 def criptografar():
-  os.system('cls')
   palavra = input("Digite a palavra: ").upper()
-  alfabeto = {' ': 0,'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
   matriz_palavra_em_num = np.array([[], []])
-  letras = []
 
+  palavra_impar = False
   if len(palavra) % 2 != 0:
       palavra += "A"
+      palavra_impar = True
 
-  for letra in range(len(palavra)):
-      for chave, valor in alfabeto.items():
-          if palavra[letra] == chave:
-              letras.append(valor)
+  matriz_palavra_em_num = palavra_em_matriz(palavra)
 
-  for i in range(0, len(palavra)-1, 2):  
-      novaColuna = np.array([[letras[i]], [letras[i+1]]])
-      matriz_palavra_em_num = np.append(matriz_palavra_em_num, novaColuna, axis=1)
-
+  palavra = palavra[:-1]
   print("-"*50)
   print(f"Matriz da palavra  {palavra} em número:")
   print(matriz_palavra_em_num)
@@ -27,45 +21,15 @@ def criptografar():
 
   chaveMatriz = np.array([[4, 3], [1, 2]])
   criptografada = np.dot(chaveMatriz, matriz_palavra_em_num) % 26
-
-
   print(f"Matriz da palavra  {palavra} criptografada:")
   print(criptografada)
   print("-"*50)
-
-  palavra_criptografada = ''
-  for linha in criptografada.T:
-      for num in linha:
-          if num == 0:  
-              palavra_criptografada += 'Z'
-          else:
-              for chave, valor in alfabeto.items():
-                  if num == valor:
-                      palavra_criptografada += chave
-
-  if palavra[-1:] == " ":
-      palavra_criptografada = palavra_criptografada[:-1]
-
-  print(f" {palavra} == {palavra_criptografada}")
+  print(f" {palavra} == {monta_palavra(criptografada, palavra_impar)}")
 
 def decifrar():
   palavra_criptograda = input("Digite a palavra criptografada: ").upper()
-  alfabeto = {' ': 0,'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
-  matriz_palavra_em_num = np.array([[], []])
-  letras = []
 
-  if len(palavra_criptograda) % 2 != 0:
-      palavra_criptograda += " "
-
-  for letra in range(len(palavra_criptograda)):
-      for chave, valor in alfabeto.items():
-          if palavra_criptograda[letra] == chave:
-              letras.append(valor)
-
-  for i in range(0, len(palavra_criptograda)-1, 2):  
-      novaColuna = np.array([[letras[i]], [letras[i+1]]])
-      matriz_palavra_em_num = np.append(matriz_palavra_em_num, novaColuna, axis=1)
-
+  matriz_palavra_em_num = palavra_em_matriz(palavra_criptograda)
   a = 4
   b = 3
   c = 1
@@ -73,34 +37,46 @@ def decifrar():
   chaveMatrizInversa = np.array([[d, -b], [-c, a]])
   det = (a*d)-(c*b)
   det_inversas = {1: 1, 3: 9, 5: 21, 7: 15, 9: 3, 11: 19, 15: 7, 17: 23, 19: 11, 21: 5, 23: 17, 25: 25}
-
   for chave, valor in det_inversas.items():
      if det == chave:
         det = valor
         break
-     
   chaveMatrizInversa *= det
   print('-'*50) 
   print(chaveMatrizInversa)
   print('-'*50) 
   print(matriz_palavra_em_num)
-
   matriz_palavra_em_num = np.dot(chaveMatrizInversa, matriz_palavra_em_num)%26
-
   print('-'*50) 
   print(matriz_palavra_em_num)
   print('-'*50)
-  palavra_decifrada = ''
-  for linha in matriz_palavra_em_num.T:
+  print(f" {palavra_criptograda} == {monta_palavra(matriz_palavra_em_num, False)}") 
+
+def palavra_em_matriz(palavra):
+  letras = []
+  matriz_palavra_em_num = np.array([[], []])
+  for letra in range(len(palavra)):
+    for chave, valor in alfabeto.items():
+      if palavra[letra] == chave:
+          letras.append(valor)
+  for i in range(0, len(palavra)-1, 2):  
+      novaColuna = np.array([[letras[i]], [letras[i+1]]])
+      matriz_palavra_em_num = np.append(matriz_palavra_em_num, novaColuna, axis=1)
+  return matriz_palavra_em_num
+
+def monta_palavra(matriz, palavra_impar):
+  palavra_formada = ''
+  for linha in matriz.T:
       for num in linha:
           if num == 0:  
-              palavra_decifrada += 'Z'
+              palavra_formada += 'Z'
           else:
               for chave, valor in alfabeto.items():
                   if num == valor:
-                      palavra_decifrada += chave
-
-  print(f" {palavra_criptograda} == {palavra_decifrada}") 
+                      palavra_formada += chave
+  if palavra_impar is True:
+    palavra_formada = palavra_formada[:-1]
+  return palavra_formada
 
 def menu():
   while True:
@@ -119,6 +95,6 @@ def menu():
       os.system('cls')
       print('-'*10, 'Digite uma opção', '-'*10)
       continue
-      
+
 if __name__ == '__main__':
   menu()
--- [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/teste.py
+++ [Algebra Linear]/[01.Aplicacao_em_Softw.]/[Vetores e matrizes]/[Criptografia]/teste.py
@@ -1,50 +1,55 @@
 import numpy as np
 import os
 
-def text_to_matrix(text):
-    text = text.upper().replace(" ", "")
-    matrix = []
-    for char in text:
-        matrix.append(ord(char) - ord('A') + 1)  # Convertendo letras para números de 1 a 26
-    matrix = np.array(matrix)
-    if len(matrix) % 2 != 0:
-        matrix = np.append(matrix, [0])  # Se ímpar, adiciona 'A' no final
-    matrix = matrix.reshape(-1, 2).T  # Matriz 2xN
-    return matrix
-
-def matrix_to_text(matrix):
-    text = ''
-    for num in matrix.flatten():
-        if num == 0:
-            text += ' '
-        else:
-            text += chr(num + ord('A') - 1)
-    return text
+alfabeto = {' ': 0,'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10, 'K': 11, 'L': 12, 'M': 13, 'N': 14, 'O': 15, 'P': 16, 'Q': 17, 'R': 18, 'S': 19, 'T': 20, 'U': 21, 'V': 22, 'W': 23, 'X': 24, 'Y': 25, 'Z': 26}
 
 def criptografar():
-    os.system('cls')
-    palavra = input("Digite a palavra: ")
-    matriz_palavra_em_num = text_to_matrix(palavra)
+    palavra = input("Digite a palavra: ").upper()
+    if len(palavra) % 2 != 0:
+        palavra += "A"
+    matriz_palavra_em_num = palavra_em_matriz(palavra)
+
+    print("-"*50)
+    print(f"Matriz da palavra  {palavra} em número:")
+    print(matriz_palavra_em_num)
+    print("-"*50)
+
     chaveMatriz = np.array([[4, 3], [1, 2]])
     criptografada = np.dot(chaveMatriz, matriz_palavra_em_num) % 26
-    palavra_criptografada = matrix_to_text(criptografada)
-    print("-"*50)
-    print(f"Matriz da palavra {palavra} criptografada:")
+
+    print(f"Matriz da palavra  {palavra} criptografada:")
     print(criptografada)
     print("-"*50)
-    print(f"{palavra} == {palavra_criptografada}")
+
+    print(f" {palavra} == {monta_palavra(criptografada)}")
 
 def decifrar():
-    palavra_criptografada = input("Digite a palavra criptografada: ")
-    matriz_palavra_em_num = text_to_matrix(palavra_criptografada)
-    chaveMatrizInversa = np.array([[2, -3], [-1, 4]])  # Inversa da matriz chave
-    decifrada = np.dot(chaveMatrizInversa, matriz_palavra_em_num) % 26
-    palavra_decifrada = matrix_to_text(decifrada)
-    print("-"*50)
-    print(f"Matriz da palavra criptografada {palavra_criptografada} decifrada:")
-    print(decifrada)
-    print("-"*50)
-    print(f"{palavra_criptografada} == {palavra_decifrada}")
+    palavra_criptografada = input("Digite a palavra criptografada: ").upper()
+    if len(palavra_criptografada) % 2 != 0:
+        palavra_criptografada += "A"
+    matriz_palavra_em_num = palavra_em_matriz(palavra_criptografada)
+
+    chaveMatrizInversa = np.array([[2, -3], [-1, 4]])
+    det = 8
+    matriz_palavra_em_num = np.dot(chaveMatrizInversa, matriz_palavra_em_num) % 26
+
+    print('-'*50) 
+    print(matriz_palavra_em_num)
+    print('-'*50)
+
+    print(f" {palavra_criptografada} == {monta_palavra(matriz_palavra_em_num)}")
+
+def palavra_em_matriz(palavra):
+    letras = [alfabeto[ch] for ch in palavra]
+    matriz_palavra_em_num = np.array(letras).reshape(-1, 2).T
+    return matriz_palavra_em_num
+
+def monta_palavra(matriz):
+    palavra_formada = ''
+    for linha in matriz.T:
+        for num in linha:
+            palavra_formada += chr(num + 64) if num != 0 else 'Z'
+    return palavra_formada
 
 def menu():
     while True:
@@ -61,8 +66,8 @@ def menu():
             break
         else:
             os.system('cls')
-            print('-'*10, 'Digite uma opção válida', '-'*10)
+            print('-'*10, 'Digite uma opção', '-'*10)
             continue
-
+      
 if __name__ == '__main__':
     menu()
